b58168f6f2acee616392025092925ed4ba456e48
Ties van de Ven <ties@topicus.nl>
2013-11-01 22:22:27 +0100

    basis html

---
+++ scripts/view.js
@@ -0,0 +1,15 @@
+/**
+ * Created by ties on 1-11-13.
+ */
+(function(ha){
+    ha.view = {
+        updateZielenCount : function(zielenCount){
+            $('zielenCount').html(zielenCount);
+        },
+        updateGeldCount : function(geldCount){
+            $('geldCount').html(geldCount);
+        }
+    };
+}(Hades));/**
+ * Created by ties on 1-11-13.
+ */
