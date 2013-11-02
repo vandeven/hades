60949040773c1622e0935121cbc5a608d3cd09d5
Ties van de Ven <ties@topicus.nl>
2013-11-02 22:38:19 +0100

    wtf weirde shit

--- assets/scripts/init.js
+++ assets/scripts/init.js
@@ -11,7 +11,7 @@
 
         self.generateMap(50,35);
         self.generateMenu();
-//        self.generateAdvert();
+        self.generateAdvert();
 
         $('.cell').each(function(i, cell){
             $(cell).droppable({
@@ -133,7 +133,7 @@
         Hades.decreaseSouls(soulCost);
         building.destroy();
         Hades.view.destroyBuilding(cell);
-    } ;
+    };
 }(Hades));
 
 
