80dfb70380a376f3061edc8e3aca2a9fb4f717b3
Ties van de Ven <ties@topicus.nl>
2013-11-02 16:36:40 +0100

    menu in apart js bestand zetten

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -97,28 +97,6 @@ var Hades = {
         self.view.setSoulCount(this.counters.souls);
         self.disableBuildings();
     },
-    enableBuildings : function(){
-        var self = this;
-        for(var i = 0; i < Hades.buildings.length; i++){
-            var buildingId = Hades.buildings[i];
-            var buildingCost = self.getBuildingCostById(buildingId);
-            var soulCost = self.getBuildingSoulCostById(buildingId);
-            if(self.counters.money >= buildingCost && self.counters.souls >= soulCost){
-                Hades.view.enableBuilding(buildingId);
-            }
-        }
-    },
-    disableBuildings : function(){
-        var self = this;
-        for(var i = 0; i < Hades.buildings.length; i++){
-            var buildingId = Hades.buildings[i];
-            var buildingCost = self.getBuildingCostById(buildingId);
-            var soulCost = self.getBuildingSoulCostById(buildingId);
-            if(this.counters.money < buildingCost || self.counters.souls < soulCost){
-                Hades.view.disableBuilding(buildingId);
-            }
-        }
-    },
     getBuildingCostById : function(id){
         if(id === Hades.moneyBuildingId){
             return 10;
